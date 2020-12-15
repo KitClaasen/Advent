66d05c6e129cf9623549b9754c763bb2cd412d9e
Kit Claasen <kit@claasen.biz>
2020-12-15 14:47:51 +0000

    Solves Day01

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -7,12 +7,13 @@ namespace Advent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var day01 = new Day01();
             day01.Solve();
 
-            var input = Properties.Resources.Year2020Day01;
-            var lines =input.Split(Environment.NewLine);
+
+
+
+
         }
     }
 }
--- Advent/Year2020/Day01.cs
+++ Advent/Year2020/Day01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Advent.Year2020
@@ -8,7 +9,82 @@ namespace Advent.Year2020
     {
         public void Solve()
         {
-            throw new NotImplementedException();
+            var input = Properties.Resources.Year2020Day01;
+            var lines = input
+                .Split(Environment.NewLine)
+                .Select(entry => int.Parse(entry))
+                .ToArray();
+
+            var val1 = FindProdPart1(lines);
+            var val2 = FindProdPart2(lines);
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+        }
+ 
+
+        private int FindProdPart1(int[] entries)
+        {
+            const int result = 2020;
+            int i = 0,
+                prod = 0;
+            var found = false;
+
+            while (i < entries.Length - 2)
+            {
+                if (found) break;
+                int j = i + 1;
+
+                while (j < entries.Length - 1)
+                {
+                    if (entries[i] + entries[j] == result)
+                    {
+                        found = true;
+                        prod = entries[i] * entries[j];
+                    }
+
+                    if (found) break;
+                    j++;
+                }
+                i++;
+            }
+            return prod;
+        }
+
+
+        private int FindProdPart2(int[] entries)
+        {
+            const int result = 2020;
+            int i = 0,
+                prod = 0;
+            var found = false;
+
+
+            while (i < entries.Length - 2)
+            {
+                if (found) break;
+                int j = i + 1;
+
+                while (j < entries.Length - 1)
+                {
+                    if (found) break;
+                    int k = j + 1;
+
+                    while (k < entries.Length)
+                    {
+                        if (entries[i] + entries[j] + entries[k] == result)
+                        {
+                            found = true;
+                            prod = entries[i] * entries[j] * entries[k];
+                        }
+
+                        if (found) break;
+                        k++;
+                    }
+                    j++;
+                }
+                i++;
+            }
+            return prod;
         }
     }
 }
