4b132f7e6a83a70f8024de95f1f1b035e17281be
Kit Claasen <kit@claasen.biz>
2020-12-15 12:20:13 +0000

    Basic git repo set up and ready to use

---
+++ Advent/Program.cs
@@ -0,0 +1,18 @@
+﻿using Advent.Year2020;
+using System;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var day01 = new Day01();
+            day01.Solve();
+
+            var input = Properties.Resources.Year2020Day01;
+            var lines =input.Split(Environment.NewLine);
+        }
+    }
+}
---
+++ Advent/Year2020/Day01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent.Year2020
+{
+    public class Day01 : IDay
+    {
+        public void Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
